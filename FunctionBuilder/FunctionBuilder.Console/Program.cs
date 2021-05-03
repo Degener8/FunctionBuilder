8326c5beaefa83a9a8772ffa20f777902219721c
Degener8 <raptoroidinbox@gmail.com>
2021-05-03 22:50:53 +0500

    init

---
+++ FunctionBuilder/FunctionBuilder.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionBuilder.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
